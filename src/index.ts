47069402a4ac7904df08031061e1a14a24554fb9
Anushk Farkiya <anushk.farkiya@gmail.com>
2025-05-13 18:40:23 +0530

    added flow apis

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import userRoutes from './routes/userRoutes.js';
 import interestRoutes from './routes/interestRoutes.js';
 import topicRoutes from './routes/topicRoutes.js';
 import wordRoutes from './routes/wordRoutes.js';
+import flowRoutes from './routes/flowRoutes.js';
 
 dotenv.config({ path: path.resolve('.env') });
 
@@ -29,6 +30,7 @@ app.use('/user', userRoutes);
 app.use('/interest', interestRoutes);
 app.use('/topic', topicRoutes);
 app.use('/word', wordRoutes);
+app.use('/flow', flowRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
